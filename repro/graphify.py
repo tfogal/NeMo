55c88c91c0f0831cbb2ec92b6bf20b47fd3ef979
Tom Fogal <60981+tfogal@users.noreply.github.com>
2024-09-17 11:27:40 -0700

    fix graphify according to Kshiteej's input

--- repro/graphify.py
+++ repro/graphify.py
@@ -7,6 +7,14 @@ import torch._inductor
 import torch._inductor.cudagraph_trees
 import torch._inductor.compile_fx
 
+#def foo(args: list[torch.Tensor]) -> torch.Tensor:
+#  a = args[0]
+#  b = args[1]
+#  t0 = a + 0.4242
+#  t1 = t0 * b
+#  t2 = torch.div(t1, 1.48)
+#  return t2
+
 def foo(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
   t0 = a + 0.4242
   t1 = t0 * b
@@ -22,11 +30,12 @@ def cugraph(gm: torch.fx.GraphModule, args: list[torch.Tensor], **kwargs):
     "is_inference": False,
   }
   try: # ensure we only get *our* traceback when it dies
-    graph_it = torch._inductor.compile_fx.cudagraphify(
-      gm,
+    graph_it = torch._inductor.cudagraph_trees.cudagraphify(
+      gm
+      inputs=args,
+      static_input_idxs=[],
       **keywords,
     )
-    print(f"args: {args}")
     #print(help(graph_it))
     rv = graph_it(args) # force it to graph now.
     #return rv
@@ -35,8 +44,25 @@ def cugraph(gm: torch.fx.GraphModule, args: list[torch.Tensor], **kwargs):
     traceback.print_exception(e)
     sys.exit(42)
 
+def cugraph2(gm: torch.fx.GraphModule, args: list[torch.Tensor], **kwargs):
+  gm.real_recompile()
+  try: # ensure we only get *our* traceback when it dies
+    g = torch.cuda.CUDAGraph()
+    with torch.cuda.graph(g):
+      o = gm(*args)
+    def wrapper(*args, **kwargs):
+      g.replay()
+      return o
+
+    return wrapper
+  except Exception as e:
+    traceback.print_exception(e)
+    sys.exit(42)
+  return None
+
 fqn = torch.compile(foo, backend=cugraph)
-fqn(
+inputs = [
   torch.randn((2), device='cuda'),
   torch.randn((2), device='cuda')
-)
+]
+fqn(*inputs)
